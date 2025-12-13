b513f4c013b81bed8f9658b7c39e4f063cbb3caf
chaitanya-1509 <krishnachaitanyamundluru@gmail.com>
2025-12-13 13:49:04 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
