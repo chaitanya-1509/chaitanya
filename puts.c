5ccddc856b3af76fbd1558cbe74f149c0e7a9431
chaitanya-1509 <krishnachaitanyamundluru@gmail.com>
2025-12-25 17:35:16 +0530

    Implement name input and display in puts.c
    This program prompts the user to enter a student's name and their father's name, then displays both names using puts function.

---
+++ puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char sname[20];
+    char fname[20];
+    printf("Enter student name : ");
+    scanf("%s",&sname);
+    printf("Enter father name : ");
+    scanf("%s",&fname);
+    puts(sname);
+    puts(fname);
+}
